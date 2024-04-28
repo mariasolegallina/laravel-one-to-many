f12265e32ebc4f87e29f7070a7e3e62981986521
mariasolegallina <gallinamariasole@gmail.com>
2024-04-28 18:46:24 +0200

    add - type table, migration, seeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = [
+            'FrontEnd',
+            'BackEnd',
+            'FullStack',
+            'UI',
+            'UX'
+        ];
+
+        foreach ($types as $type) {
+
+            $new_type = new Type();
+            $new_type->name  = $type;
+
+            $new_type->save();
+        }
+    }
+}
